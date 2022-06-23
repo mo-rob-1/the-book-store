cf325af3e85cf59e7db6a604e2bcdeb7cc1c7cb1
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-06-24 00:47:57 +0100

    add margin classes

--- src/components/SocialMediaLinks.jsx
+++ src/components/SocialMediaLinks.jsx
@@ -4,17 +4,23 @@ function SocialMediaLinks() {
   const style = { color: "black", fontSize: "2.6rem" };
 
   return (
-    <div className="socials__wrapper">
-      <a href="https://twitter.com/">
-        <FaTwitter style={style} />
-      </a>
-      <a href="https://www.facebook.com/">
-        <FaFacebookSquare style={style} />
-      </a>
-      <a href="https://www.instagram.com/">
-        <FaInstagram style={style} />
-      </a>
-    </div>
+    <ul className="socials__wrapper">
+      <li className="socials--margin">
+        <a href="https://twitter.com/">
+          <FaTwitter style={style} />
+        </a>
+      </li>
+      <li className="socials--margin">
+        <a href="https://www.facebook.com/">
+          <FaFacebookSquare style={style} />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.instagram.com/">
+          <FaInstagram style={style} />
+        </a>
+      </li>
+    </ul>
   );
 }
 
