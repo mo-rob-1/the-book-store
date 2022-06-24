73d7b8b34923e53eac9cb24c180ba21a981b47b7
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-06-24 13:02:49 +0100

    debug

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -5,6 +5,7 @@ function Books() {
   const [data, setData] = useState([]);
   const [item, setItem] = useState(localStorage.getItem("active") === "true");
   const [isLoading, setIsLoading] = useState(true);
+  const [reverse, setReverse] = useState([]);
 
   useEffect(() => {
     axios
@@ -12,6 +13,7 @@ function Books() {
       .then((res) => {
         setData(res.data.items);
         setIsLoading(false);
+        setReverse(res.data.items.reverse());
       })
       .catch((err) => {
         console.log(err);
@@ -34,21 +36,44 @@ function Books() {
   }
 
   return (
-    <ul className="books">
-      {data.map((item) => (
-        <li key={item.etag} className="books__item" onClick={isSelected}>
-          <img src={item.volumeInfo.imageLinks.thumbnail} alt={item.volumeInfo.title} className="books__item-img" />
-          <div className="books__item-text-wrapper">
-            <h3 className="books__item-title">{item.volumeInfo.title}</h3>
-            <p className="books__item-author">{Object.values(item.volumeInfo.authors || {}).join(", ")}</p>
-            <p className="books__item-pages">
-              <b>Pages:</b> {item.volumeInfo.pageCount}
-            </p>
-            <p className="books__item-desc">{item.volumeInfo.description.substring(0, 140)}...</p>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <div className="two-col">
+      <ul className="books">
+        {data.map((item) => (
+          <li key={item.etag} className="books__item" onClick={isSelected}>
+            <img src={item.volumeInfo.imageLinks.thumbnail} alt={item.volumeInfo.title} className="books__item-img" />
+            <div className="books__item-text-wrapper">
+              <h3 className="books__item-title">{item.volumeInfo.title}</h3>
+              <p className="books__item-author">{Object.values(item.volumeInfo.authors || {}).join(", ")}</p>
+              <p className="books__item-pages">
+                <b>Pages:</b> {item.volumeInfo.pageCount}
+              </p>
+              <p className="books__item-desc">{item.volumeInfo.description.substring(0, 140)}...</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <aside className="featured">
+        <h2 className="featured__title">Featured</h2>
+        <ul className="featured__list">
+          {reverse
+            .reverse()
+            .slice(0, 2)
+            .map((item) => (
+              <li key={item.etag} className="featured__list-item" onClick={isSelected}>
+                <div className="featured__list-item-img">
+                  <img src={item.volumeInfo.imageLinks.thumbnail} alt={item.volumeInfo.title} />
+                </div>
+                <h3>{item.volumeInfo.title}</h3>
+                <p className="author-name">{Object.values(item.volumeInfo.authors || {}).join(", ")}</p>
+                <p className="number-of-pages">
+                  <b>Pages:</b> {item.volumeInfo.pageCount}
+                </p>
+                <p className="desc">{item.volumeInfo.description.substring(0, 140)}...</p>
+              </li>
+            ))}
+        </ul>
+      </aside>
+    </div>
   );
 }
 
