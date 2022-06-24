995ab44c11c9425dadb22cdae7bc51d88420b54b
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-06-24 13:08:28 +0100

    debug

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -5,7 +5,6 @@ function Books() {
   const [data, setData] = useState([]);
   const [item, setItem] = useState(localStorage.getItem("active") === "true");
   const [isLoading, setIsLoading] = useState(true);
-  const [reverse, setReverse] = useState([]);
 
   useEffect(() => {
     axios
@@ -13,7 +12,6 @@ function Books() {
       .then((res) => {
         setData(res.data.items);
         setIsLoading(false);
-        setReverse(res.data.items.reverse());
       })
       .catch((err) => {
         console.log(err);
@@ -36,7 +34,7 @@ function Books() {
   }
 
   return (
-    <div className="two-col">
+    <div>
       <ul className="books">
         {data.map((item) => (
           <li key={item.etag} className="books__item" onClick={isSelected}>
@@ -52,27 +50,6 @@ function Books() {
           </li>
         ))}
       </ul>
-      <aside className="featured">
-        <h2 className="featured__title">Featured</h2>
-        <ul className="featured__list">
-          {reverse
-            .reverse()
-            .slice(0, 2)
-            .map((item) => (
-              <li key={item.etag} className="featured__list-item" onClick={isSelected}>
-                <div className="featured__list-item-img">
-                  <img src={item.volumeInfo.imageLinks.thumbnail} alt={item.volumeInfo.title} />
-                </div>
-                <h3>{item.volumeInfo.title}</h3>
-                <p className="author-name">{Object.values(item.volumeInfo.authors || {}).join(", ")}</p>
-                <p className="number-of-pages">
-                  <b>Pages:</b> {item.volumeInfo.pageCount}
-                </p>
-                <p className="desc">{item.volumeInfo.description.substring(0, 140)}...</p>
-              </li>
-            ))}
-        </ul>
-      </aside>
     </div>
   );
 }
